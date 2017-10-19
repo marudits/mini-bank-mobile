52b2f8174e29c712d72e0d19f167be609b0fb5f2
marudits <marudits@gmail.com>
2017-10-19 13:37:22 +0800

    :sparkles: NEW FEATURES | Contact List and Search Function

---
+++ src/components/employee/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+	id: number;
+	name: string;
+	email: string;
+	phone: string;
+	ext: string;
+	departmentId: number;
+	positionId: number;
+	updatedAt: string;
+	
+	constructor(){}
+}
